d71e2adb3edfde586a4a39602f093bed1042f972
caperaven@icloud.com <caperaven@icloud.com>
2016-07-01 10:16:52 +0200

    animation

--- src/views/welcome/welcome.ts
+++ src/views/welcome/welcome.ts
@@ -2,7 +2,6 @@ import {bindable} from 'aurelia-framework';
 
 export class Welcome {
     @bindable items;
-    svg;
 
     constructor() {
         this.items = [
@@ -23,16 +22,5 @@ export class Welcome {
                 name: "Tab 4"
             }]
 
-    }
-
-    bind() {
-        this.svg.addEventListener('click', this.animateSvg);
-    }
-
-    animateSvg() {
-        requestAnimationFrame(function() {
-            const circle = document.getElementById('circle');
-            circle.style.transform = 'translateX(300px)';
-        })
     }
 }
